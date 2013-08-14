179ab0ff68dafd1569494b6f05134606797d506b
Brian Cain <brianccain@gmail.com>
2013-08-14 16:10:03 -0700

    Update sbt.sh

--- jenkins-integration/scripts/sbt.sh
+++ jenkins-integration/scripts/sbt.sh
@@ -10,4 +10,4 @@ export PUPPET_GATLING_MASTER_BASE_URL=https://$MASTER:8140
 export PUPPET_GATLING_SIMULATION_CONFIG=./config/scenarios/$SCENARIO
 
 cd ../simulation-runner
-java -Xss2M -Dsbt.log.noformat=true -jar $SBTPATH run
+java -Xss2M -Xmx512M -Dsbt.log.noformat=true -jar $SBTPATH run
